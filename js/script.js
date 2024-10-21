be3f942d49756652fb62cebc9cca4bd93d090e1c
itsme442 <eliaszeruasegahegn@gmail.com>
2024-10-21 22:06:31 +0200

    first commit

--- js/script.js
+++ js/script.js
@@ -1,117 +1,96 @@
 const canvas = document.querySelector(".canvas");
 const ctx = canvas.getContext("2d");
 
-console.log(ctx);
-
 const scale = 20;
 const rows = canvas.height / scale;
 const columns = canvas.width / scale;
 
-
 let score = 0; 
 let snake = [];
 
-snake[0]= {
-  x : (Math.floor(Math.random() *
-    columns)) * scale,
-  y : (Math.floor(Math.random() *
-    rows)) * scale
+snake[0] = {
+  x: (Math.floor(Math.random() * columns)) * scale,
+  y: (Math.floor(Math.random() * rows)) * scale
 };
-console.log(snake);
 
 let food = {
-  x : (Math.floor(Math.random() *
-    columns)) * scale, 
-  y : (Math.floor(Math.random() *
-    rows)) * scale
-}
+  x: (Math.floor(Math.random() * columns)) * scale, 
+  y: (Math.floor(Math.random() * rows)) * scale
+};
 
-let playGame = setInterval(draw,100);
-
+let playGame = setInterval(draw, 100);
 
 let d = "right";
 
 document.onkeydown = direction;
 
-function direction(event){
-let key = event.keyCode;
-    if( key == 37 && d != "right"){
-    d = "left";
-}else if(key == 38 && d != "down"){
-    d = "up";
-}else if(key == 39 && d != "left"){
-    d = "right";
-}else if(key == 40 && d != "up"){
-    d = "down";
+function direction(event) {
+  let key = event.keyCode;
+  if (key == 37 && d != "right") d = "left";
+  else if (key == 38 && d != "down") d = "up";
+  else if (key == 39 && d != "left") d = "right";
+  else if (key == 40 && d != "up") d = "down";
 }
-}
+
 function draw() {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	for (let i=0; i<snake.length; i++) {
-		ctx.fillStyle = "#fff";
-		ctx.strokeStyle = "red";
-	ctx.fillRect(snake[i].x,
-	snake[i].y, scale, scale);
-    ctx.strokeRect(snake[i].x,snake[i].y,scale,scale);  
-	}
-	console.log(snake);
-	
-	ctx.fillStyle = "#ff0";
-	ctx.strokeStyle = "green";
-	ctx.fillRect(food.x, food.y, scale, scale);
-	ctx.strokeRect(food.x, food.y,scale,scale);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  
+  for (let i = 0; i < snake.length; i++) {
+    ctx.fillStyle = "#fff";
+    ctx.strokeStyle = "red";
+    ctx.fillRect(snake[i].x, snake[i].y, scale, scale);
+    ctx.strokeRect(snake[i].x, snake[i].y, scale, scale);  
+  }
 
-let snakeX = snake[0].x;
-let snakeY = snake[0].y;
+  ctx.fillStyle = "#ff0";
+  ctx.strokeStyle = "green";
+  ctx.fillRect(food.x, food.y, scale, scale);
+  ctx.strokeRect(food.x, food.y, scale, scale);
 
-console.log(snakeX);
-if( d == "left") snakeX -= scale;
-if( d == "up") snakeY -= scale;
-if( d == "right") snakeX += scale;
-if( d == "down") snakeY += scale;
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
 
-if (snakeX > canvas.width) {
-    snakeX = 0;
-}
-if (snakeY > canvas.height) {
-    snakeY = 0;
-}
-if (snakeX < 0) {
-    snakeX = canvas.width;
-}
-if (snakeY < 0) {
-    snakeY = canvas.height;
-}
+  if (d == "left") snakeX -= scale;
+  if (d == "up") snakeY -= scale;
+  if (d == "right") snakeX += scale;
+  if (d == "down") snakeY += scale;
 
-if(snakeX == food.x && snakeY == food.y){
+  if (snakeX >= canvas.width) snakeX = 0;
+  if (snakeY >= canvas.height) snakeY = 0;
+  if (snakeX < 0) snakeX = canvas.width;
+  if (snakeY < 0) snakeY = canvas.height;
+
+  if (snakeX == food.x && snakeY == food.y) {
     score++;
     food = {
-          x : (Math.floor(Math.random() * columns)) * scale,
-          y : (Math.floor(Math.random() * rows)) * scale
-}
-
-}else{
-    
+      x: (Math.floor(Math.random() * columns)) * scale,
+      y: (Math.floor(Math.random() * rows)) * scale
+    };
+  } else {
     snake.pop();
-}
-console.log(snake);
+  }
 
-let newHead = {
-    x : snakeX,
-    y : snakeY
-}
-console.log(snake);
-if(eatSelf(newHead,snake)){
-clearInterval(playGame);
-}
-snake.unshift(newHead);
+  let newHead = { x: snakeX, y: snakeY };
+
+  if (eatSelf(newHead, snake)) {
+    clearInterval(playGame);
+  }
+
+  snake.unshift(newHead);
+  drawScore();
 }
 
-function eatSelf(head,array){
-for(let i = 0; i < array.length; i++){
-    if(head.x == array[i].x && head.y == array[i].y){
-            return true;
+function eatSelf(head, array) {
+  for (let i = 0; i < array.length; i++) {
+    if (head.x == array[i].x && head.y == array[i].y) {
+      return true;
+    }
+  }
+  return false;
 }
-}
-return false;
+
+function drawScore() {
+  ctx.fillStyle = "black";
+  ctx.font = "20px Arial";
+  ctx.fillText("Score: " + score, 10, 20);
 }
